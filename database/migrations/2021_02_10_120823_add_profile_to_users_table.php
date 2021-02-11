c0b7f29eb975f8249e4790543785487be0feb0ba
Ronald Tchuekou <54064527+ronald-tchuekou@users.noreply.github.com>
2021-02-11 21:13:30 +0100

    Modification de la table user.profile.

--- database/migrations/2021_02_10_120823_add_profile_to_users_table.php
+++ database/migrations/2021_02_10_120823_add_profile_to_users_table.php
@@ -14,7 +14,7 @@ class AddProfileToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('profile')->default('none');
+            $table->text('profile')->nullable();
         });
     }
 
