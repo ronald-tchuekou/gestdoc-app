8f9d4394382cf28998165bfd75cd812c7cf88aba
ronald-tchuekou <ronaldtchuekou@gmail.com>
2021-02-25 22:32:22 +0100

    New push

--- public/js/rest.api.js
+++ public/js/rest.api.js
@@ -1,3 +1,3 @@
-const HOST_BACKEND = 'http://localhost:8000',
-// const HOST_BACKEND = 'https://gestdoc-app.herokuapp.com',
+// const HOST_BACKEND = 'http://localhost:8000',
+const HOST_BACKEND = 'https://gestdoc-app.herokuapp.com',
     BACKEND = HOST_BACKEND + 'api';
