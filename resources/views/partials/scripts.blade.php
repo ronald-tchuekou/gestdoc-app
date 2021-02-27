2716e3081db0008e8097077c38e084ce381d616e
ronald-tchuekou <ronaldtchuekou@gmail.com>
2021-02-27 12:11:38 +0100

    New

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -26,61 +26,42 @@
 <script src="{{ asset('js/axios.min.js') }}"></script>
 <script src="{{ asset('js/rest.api.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('js/notifyEventListener.js') }}"></script>
-
-<script>
-    (function (window, document, $) {
-        $(document).ready(() => {
-
-            let current_account = window.location.pathname.split('/')[1];
-            let user_id = parseInt('{{Auth::id()}}');
-            var intervalID = -1;
-
-            // intervalID = setInterval(() => {
-
-            //     // GET THE HOSTNAME.
-            //     let host = window.location;
-            //     new ListenChange(current_account, user_id, intervalID);
-
-            // }, 1000);
-        });
-    })(window, document, jQuery);
-</script>
+{{-- <script src="{{ asset('js/notifyEventListener.js') }}"></script> --}}
 
 <!-- // About the home enter account. -->
 @if($current_account == 'accueil')
 
-<script>
+{{-- <script>
     $(document).ready(function () {
 
         let init_courrier_table = document.querySelector('#accueil-init-courrier')
 
         if(init_courrier_table != null) {
             setInterval(() => {
-    
+
                 axios.get(HOST_BACKEND + "/{{$current_account}}/all-init-courriers/{{Auth::id()}}")
                     .then(response => {
-                    
+
                         let result = response.data
-    
+
                         if(response.data.record != null) {
                             let row = $(`#accueil-init-courrier tr[data-row="${result.record.id}"]`)
                             // remove the row.
                             $(row).remove()
                             toastr.info(`Le courrier N° ${result.record.id} à été ${result.context}`)
                         }
-                        
+
                         console.log('The content of this page is reloaded.')
-    
+
                         console.log('The response : ', response.data)
                     }).catch(reason => {
                         console.error(reason)
                     })
-                    
+
             }, 10000);
         }
 
     })
-</script>
+</script> --}}
 
 @endif
